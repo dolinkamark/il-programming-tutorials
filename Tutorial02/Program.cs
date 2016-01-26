82a77a9afea9bb2412a2482a529633bda210e9d1
Dolinka Márk Gergely <dolinkamark@hotmail.com>
2016-01-26 16:59:31 +0100

    Add the outline of Tutorial02.

---
+++ Tutorial02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tutorial02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern void SayMyAge(int age);
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern bool IsBiggerThan(int x, int y);
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern void RepeatMe(string text, int count);
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern void Swap(object a, object b);
+	}
+}
