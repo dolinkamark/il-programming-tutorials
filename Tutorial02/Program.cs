f0eb1de61ba3b3c8344a98574fd966e60374d51a
Márk Gergely Dolinka <dolinkamark@hotmail.com>
2016-04-24 14:03:47 +0200

    Finish the example code for the 2. tutorial.

--- Tutorial02/Program.cs
+++ Tutorial02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Tutorial02
 {
@@ -7,37 +8,67 @@ namespace Tutorial02
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Conditions");
-			Console.WriteLine("----------");
-
+			WriteTitle("Conditions", false);
 			if (IsBiggerThan(10, 5))
 			{
 				Console.WriteLine("Wow, 10 is bigger than 5.");
 			}
 			
-			//Console.WriteLine(ToPower(2, 10));
+			WriteTitle("Local variables");
 			Console.WriteLine(AddWithLocal(23, 16));
+			
+			WriteTitle("Loops");
+			Console.WriteLine(ToPower(2, 10));
 
-			Console.WriteLine("Boxing");
-			Console.WriteLine("------");
-			SayMyAge(25);
+			WriteTitle("Boxing & Unboxing");
+			Boxing(25);
+
+			object boxedInt = (object)225;
+			Console.WriteLine(UnboxInt(boxedInt));
+
+			WriteTitle("Ref & out parameter modifiers");
+			int x = 10;
+			int y = 35;
+
+			SwapWithRefParameter(ref x, ref y);
+
+			Console.WriteLine("x value: {0}", x);
+			Console.WriteLine("y value: {0}", y);
 
 			Console.ReadKey();
 		}
 
-		[MethodImpl(MethodImplOptions.ForwardRef)]
-		public static extern void SayMyAge(int age);
+		private static void WriteTitle(string title, bool newLineBefore = true)
+		{
+			if (newLineBefore)
+			{
+				Console.WriteLine();
+			}
+			Console.WriteLine(title);
+			for(var i=0; i<title.Length; ++i)
+			{
+				Console.Write("-");
+			}
+
+			Console.WriteLine();
+		}
 
 		[MethodImpl(MethodImplOptions.ForwardRef)]
 		public static extern bool IsBiggerThan(int x, int y);
-
+		
 		[MethodImpl(MethodImplOptions.ForwardRef)]
-		public static extern void RepeatMe(string text, int count);
+		public static extern int AddWithLocal(int x, int y);
 
 		[MethodImpl(MethodImplOptions.ForwardRef)]
 		public static extern int ToPower(int num, int pow);
 
 		[MethodImpl(MethodImplOptions.ForwardRef)]
-		public static extern int AddWithLocal(int x, int y);
+		public static extern void Boxing(int age);
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern int UnboxInt(object toUnbox);
+
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public static extern void SwapWithRefParameter(ref int x, ref int y);
 	}
 }
